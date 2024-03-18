55031a658841f4a74a8c2d0b1e4ee78b3a4377c4
itsmiashish <ashishind63@gmail.com>
2024-03-18 12:53:38 +0530

    task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import USe from './pages/USe';
+import Useeff from './pages/Useeff';
 
 function App() {
   return (
     <div className="App">
 
-      <USe/>
+      {/* <USe/> */}
+      <Useeff/>
    {/* <h1  className='bg-slate-800' >recover topic </h1>
    <h2  className='bg-orange-700' >ya ya ya main balram </h2>
    <h3 className='bg-orange-500   ' > मैं कन्हैया छोटा भैया </h3> */}
--- src/pages/USe.js
+++ src/pages/USe.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 
 const USe = () => {
-const [count,setCount]=useState(0)
-
+const [count,setCount]=useState()
+const [balram,setbalram]=useState(' ya ya  mai balram')
+ 
   return (
 	<div>
-	  <h3>print the usestate hook {count} </h3>
+	  <h3 className='bg-slate-700 text-white border w-[200px] ' >print the usestate hook {count} </h3>
+ 
 	  <button  className='bg-green-500  rounded p-3 '  onClick={()=> setCount(count+10)}>increment</button>
 	<button className='bg-blue-400  rounded p-3 ml-4 '    onClick={()=> setCount(count-10)}   > decriment </button>
-<button  className='bg-red-700 rounded p-3  ml-[-20px]' onClick={()=>setCount(count*0)}> Rest </button>
+<button  className='bg-red-700 rounded p-3 ml-2' onClick={()=>setCount(count*0)}> Reset </button>
+	<h2 className='font-mono  text-center text-2xl ' > {balram} </h2>
+	{/* <button  className='bg-orange-600' onClick={()=> setbalram(balram==='main hu kanhayiya ')} >meme</button> */}
+	<button className='bg-orange-600' onClick={() => setbalram(balram === 'main hu kanhayiya' ? '' : 'main hu kanhayiya')}>meme</button>
 	</div>
   )
 }
