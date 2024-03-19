2dff7aafe4175f6f112ae9cb7b5118ec87a18233
itsmiashish <ashishind63@gmail.com>
2024-03-19 17:05:16 +0530

    done todo task

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import DropdownImage from './pages/DropdownImage';
+import Task from './pages/Task';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <DropdownImage/>
+<Task/>
+    {/* <DropdownImage/> */}
   </React.StrictMode>
 );
 
